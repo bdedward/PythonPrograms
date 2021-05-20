fac3e540e11bced35480f7a8512bcdcaef9ba546
bdedward <50928228+bdedward@users.noreply.github.com>
2021-05-20 15:21:47 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+FLAG = 0
+NumberChars = 256
+length = 0
+
+
+def palindrome(s):
+    count = [0] * NumberChars
+    for i in range(0, len(s)):
+        count[ord(s[i])] = count[ord(s[i])] + 1
+    odd = 0
+    for i in range(0, NumberChars):
+        if count[i] & 1:
+            odd = odd + 1
+        if odd > 1:
+            return False
+    return True
+
+
+def recursion(s):
+    global FLAG, length
+    p = palindrome(s)
+    if p and (len(s) > 1):
+        FLAG = 1
+        length = len(s)
+    if not p and FLAG == 0 and len(s) > 1:
+        for i in range(len(s)):
+            substring = s[:i] + s[i+1:]
+            recursion(substring)
+
+
+def solution(s):
+    before = len(s)
+    recursion(s)
+    return before - length
+
+
+print(solution("america"))
